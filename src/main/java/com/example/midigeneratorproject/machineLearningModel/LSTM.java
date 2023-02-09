ee99e02d5258953bb96849f240e87aacef2c6f6d
avivaviv283 <avivaviv283@gmail.com>
2023-02-09 18:20:24 +0200

    executing python file using the exec command from java

--- src/main/java/com/example/midigeneratorproject/machineLearningModel/LSTM.java
+++ src/main/java/com/example/midigeneratorproject/machineLearningModel/LSTM.java
@@ -1,5 +1,19 @@
 package com.example.midigeneratorproject.machineLearningModel;
 
+import java.io.IOException;
+
 public class LSTM {
 
+
+
+
+    public static void main(String[] args) {
+        String fetching = "python " + "C:\\Users\\WIN10\\Desktop\\test.py";
+        String[] commandToExecute = new String[]{"cmd.exe", "/c", fetching};
+        try {
+            Runtime.getRuntime().exec(commandToExecute);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
