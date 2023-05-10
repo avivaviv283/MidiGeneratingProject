8b93568f1cd3d4235675636a910d074a84ce8dca
avivaviv283 <avivaviv283@gmail.com>
2023-05-10 16:18:17 +0300

    fix for file storage service

--- src/main/java/com/example/midigeneratorproject/service/FileStorageService.java
+++ src/main/java/com/example/midigeneratorproject/service/FileStorageService.java
@@ -35,15 +35,18 @@ public class FileStorageService implements FileStorageInterface {
         temp.setFileName(file.getOriginalFilename());
 
         // Make sure uploaded file is a midi file
-        if (file.getContentType() == "mid" || file.getContentType() == "midi") {
-            try {
+        //       if (file.getContentType() == "mid" || file.getContentType() == "midi") {
+        try {
 
-                makeBlobForDatabase(temp, file);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            makeBlobForDatabase(temp, file);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return fileRepository.save(temp);
+        //      }
+        MidiFile duplicate = fileRepository.findByFileName(temp.getFileName());
+        if (duplicate == null) {
+            return fileRepository.save(temp);
+        } else return null;
     }
 
     public List<MidiFile> getFiles() {
@@ -53,6 +56,7 @@ public class FileStorageService implements FileStorageInterface {
     public MidiFile findMidiFileByName(String name) {
         return fileRepository.findByFileName(name);
     }
+
     //This service is able to translate a file into a Byte array. we can use the blob field in the database to store the file
     private static void makeBlobForDatabase(MidiFile temp, MultipartFile file) {
         byte[] imageData;
@@ -81,7 +85,7 @@ public class FileStorageService implements FileStorageInterface {
         return genreRepository.findAll();
     }
 
-    public Genre findById(Long id){
+    public Genre findById(Long id) {
         Genre returnedGenre = genreRepository.findById(id).orElse(null);
         return returnedGenre;
     }
